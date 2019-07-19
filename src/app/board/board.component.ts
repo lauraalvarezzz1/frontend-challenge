6a0016d28bcfb2968f18aec010116484d7a05475
Laura Alvarez <laura.alvarez@talosdigital.com>
2019-07-19 10:55:25 -0500

    adding functionality to validate the winner

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -16,6 +16,8 @@ export class BoardComponent implements OnInit {
   reset = true;
   controlTable = true;
   selectPlayer: string;
+  enableUndo: boolean;
+  thereIsWinner: boolean;
 
   constructor(private formBuilder: FormBuilder) {
     this.connectForm = this.formBuilder.group({
@@ -25,6 +27,8 @@ export class BoardComponent implements OnInit {
 
   ngOnInit() {
     this.showTable = false;
+    this.enableUndo = false;
+    this.thereIsWinner = false;
     this.matrix = [];
     this.selectPlayer = 'player1';
   }
@@ -54,31 +58,65 @@ export class BoardComponent implements OnInit {
     console.log(this.matrix);
   }
 
+  dropPiece(rows, columns) {
+    if (this.controlTable && this.matrix[rows][columns] === 0) {
+      this.controlTable = false;
+      this.matrix[rows][0] = this.selectPlayer;
+      this.activePice(rows, 1);
+      this.validateWinner(rows);
+
+      if (this.thereIsWinner) {
+        this.enableUndo = false;
+      } else {
+        this.enableUndo = true;
+      }
+    }
+  }
+
+  activePice(rows, columns) {
+    if (typeof this.matrix[rows][columns] !== 'undefined' && this.matrix[rows][columns] === 0 && columns <= this.columns) {
+      this.matrix[rows][columns - 1] = 0;
+      this.matrix[rows][columns] = this.selectPlayer;
+      this.activePice(rows, columns + 1);
+    } else {
+      this.controlTable = true;
+      if (this.selectPlayer === 'player2') {
+        this.selectPlayer = 'player1';
+      } else {
+        this.selectPlayer = 'player2';
+      }
+    }
+  }
+
+  validateWinner(rows) {
+    console.log(this.matrix);
+
+    // Check Vertical
+    let playerOne = 0;
+    const playerTwo = 0;
+    for (let i = 0; i < this.columns; i++) {
+      if (this.matrix[rows][i] === 'player1') {
+        playerOne = playerOne + 1;
+      } else {
+        playerOne = 0;
+      }
+
+      if (playerOne === this.connectForm.value.n) {
+        console.log ('gana jugador 1', playerOne);
+      }
+    }
+
+    console.log(playerOne, '2', playerTwo);
+  }
+
+  undoLastPlay(rows, columns) {
+    console.log(this.matrix);
+  }
+
   resetTable() {
     this.showTable = false;
     this.matrix = [];
     this.clicked = false;
     this.connectForm.reset();
   }
-
-  dropPiece(rows, columns) {
-    if (this.controlTable && this.matrix[rows][columns] === 0) {
-      this.controlTable = false;
-      this.activePice(rows, 1);
-      console.log(this.matrix);
-    }
-  }
-
-  activePice(rows, columns) {
-    if (typeof this.matrix[rows][columns] !== 'undefined' && this.matrix[rows][columns] === 0 && columns < this.columns) {
-      this.matrix[rows][columns - 1] = 0;
-      this.matrix[rows][columns] = this.selectPlayer;
-      this.activePice(rows, columns + 1);
-    } else {
-      this.selectPlayer = this.selectPlayer === 'player1' ? 'player2' : 'player1';
-      this.controlTable = true;
-    }
-
-  }
-
 }
